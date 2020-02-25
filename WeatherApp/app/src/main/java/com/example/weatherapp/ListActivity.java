ca34b9c0d474c972a27ecca2a927fb107edf1a7d
Hassan Tarek Abu Helal <hassantarek201517@gmail.com>
2020-02-25 02:01:16 +0200

    list view imgs

--- WeatherApp/app/src/main/java/com/example/weatherapp/ListActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/ListActivity.java
@@ -3,12 +3,19 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
-
+    ListView listView;
+    String[] OS={"Android","iPhone","Windows","Blackberry","Linux"};
+    Integer[] imgId=
+            {R.drawable.android,R.drawable.apple,R.drawable.windows,R.drawable.blackberry,R.drawable.linux};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        listView=(ListView)findViewById(R.id.listView);
+        CustomeListView customeListView=new CustomeListView(this,OS,imgId);
+        listView.setAdapter(customeListView);
     }
 }
